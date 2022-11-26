efb1f69daaace408eae42ac81707deee9fdedb40
TheLoloS <57485545+TheLoloS@users.noreply.github.com>
2022-11-26 20:46:28 +0100

    2.5
    -add some style to list and elements

--- src/App.js
+++ src/App.js
@@ -97,8 +97,8 @@ export default function App() {
               ) : (
                 <div className="mainCardNoLogin flex justify-center items-center">
                   <div className="card card-side bg-neutral shadow-xl">
-                    <div className="card-body">
-                      <h2 className="card-title">Zaloguj się!</h2>
+                    <div className="card-body text-center">
+                      <h2 className="card-title self-center">Zaloguj się!</h2>
                       <p>
                         Zaloguj się, Aby uzyskać dostep do Filmów, Programów,
                         seriali i wiele więcej!
--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -7,7 +7,52 @@ function SearchList({ movies }) {
   //   const [ListItems, setListItems] = useState();
   //   movies && console.log(movies);
   return (
-    <div className="flex flex-col w-full h-screen mainCardsConteiner">
+    <div className="searchList flex flex-col w-full mainCardsConteiner">
+      <div className="card p-4 w-full">
+        <h1 className="card-title self-center text-2xl">
+          Wyniki Wyszukiwania:
+        </h1>
+        <div className="divider" />
+        <div className="filterDiv w-full flex nowrap">
+          <select className="select select-primary w-1/4 max-w-xs justify-evenly">
+            <option selected disabled>
+              Typ
+            </option>
+            <option value="Wszystko">Wszystko</option>
+            <option value="Filmy">Filmy</option>
+            <option value="Seriale">Seriale</option>
+            <option value="Programy">Programy</option>
+            <option value="Gry">Gry</option>
+            <option value="Aplikacje">Aplikacje</option>
+          </select>
+          <select className="select select-primary w-1/4 max-w-xs">
+            <option selected disabled>
+              Kategoria
+            </option>
+            <option value="komedia">komedia</option>
+            <option value="thriller">thriller</option>
+            <option value="dramat">dramat</option>
+            <option value="melodramat">melodramat</option>
+            <option value="fantasy">fantasy</option>
+            <option value="fantastycznonaukowy">fantastycznonaukowy</option>
+            <option value="kryminał">kryminał</option>
+            <option value="horror">horror</option>
+            <option value="przygoda">przygoda</option>
+            <option value="muzyczny">muzyczny</option>
+            <option value="sensacyjny">sensacyjny</option>
+            <option value="wojenny">wojenny</option>
+            <option value="akcja">akcja</option>
+          </select>
+          <select className="select select-primary w-1/4 max-w-xs">
+            <option selected disabled>
+              Sortuj
+            </option>
+            <option value="akcja">Najczesciej ogladane</option>
+            <option value="akcja">Najlepiej oceniane</option>
+            <option value="akcja">Najnowsze</option>
+          </select>
+        </div>
+      </div>
       {movies &&
         movies.map((item, i) => {
           if (
